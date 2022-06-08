6df4d4dc0dd683becc430d482508631332f5e077
FlorentPernigotto <92726803+FlorentPernigotto@users.noreply.github.com>
2022-06-08 20:19:35 +0200

    recipeFactory update
    switch vers le laptop

--- scripts/fetchData.js
+++ scripts/fetchData.js
@@ -1,3 +1,24 @@
-fetch('data/recipes.json')
-  .then(response => response.json())
-  .then(json => console.log(json))
+async function getRecipes() {
+    // On récupère les données dans le fichiers JSON //
+    const response = await fetch('data/recipes.json');
+    if (!response.ok) {
+        return "error";
+    } else {
+        const data = await response.json();
+        return {
+            recipes: data.recipes,
+        };
+    }
+}
+ async function displayData(recipes) {
+     const recipesGallery = document.querySelector("#recipe_gallery");
+
+     // Créer un element article avec les données pour chaques photographe
+     recipes.foreach((recipe) => {
+        const recipeModel = recipeFactory(recipe);
+        const recipeCardDOM = recipeModel.
+     })
+ }
+
+// .then(response => response.json())
+// .then(json => console.log(json))
